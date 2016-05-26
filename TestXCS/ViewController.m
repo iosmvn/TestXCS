00c1e53ac71627d12d81d80da4cc56afa02be6d5
孙丹龙 <sundanlong@gmail.com>
2016-05-26 17:00:59 +0800

    first commit

---
+++ TestXCS/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  TestXCS
+//
+//  Created by sundanlong on 16/5/26.
+//  Copyright © 2016年 sundanlong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor purpleColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
